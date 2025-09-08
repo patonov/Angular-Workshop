d6521fe0f508be14146b708ada778e998dcbc5c5
Nikolay Patonov <patonec@mail.bg>
2025-09-08 23:18:01 +0300

    amend0060012

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: '**', redirectTo: '/404' },
+    { path: '404', component: ErrorComponent } 
+];
