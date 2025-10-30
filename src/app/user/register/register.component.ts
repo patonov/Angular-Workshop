633011440598d001360f9daababdab4893dc0964
Nikolay Patonov <patonec@mail.bg>
2025-10-30 21:51:01 +0200

    amend0130001

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+    form = new FormGroup({
+      username: new FormControl(''),
+      email: new FormControl(''),
+      tel: new FormControl(''),
+    });
 }
