0c92a0121e7f4ca34c446321c0d60ab36021a924
Nikolay Patonov <patonec@mail.bg>
2025-11-20 21:56:08 +0200

    amend0150001

--- src/app/theme/add-theme/add-theme.component.ts
+++ src/app/theme/add-theme/add-theme.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-theme',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-theme.component.html',
   styleUrl: './add-theme.component.css'
 })
 export class AddThemeComponent {
   constructor(private apiService: ApiService) { }
 
-  addTheme(event: Event, themeName: string, postText: string) {
-    event.preventDefault();
-    
-    this.apiService.createTheme(themeName, postText).subscribe(data => {
-      console.log(data);
-    });
+  addTheme(form: NgForm) {
+    console.log(form);
+
+    if(form.invalid){
+      return;
+    }
+
+    console.log(form.value);
+
+    // this.apiService.createTheme(themeName, postText).subscribe(data => {
+    //   console.log(data);
+    // });
   }
 
 }
