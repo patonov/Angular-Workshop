b4a66e1d5c971cf76b6850da27a68091d439aba1
Nikolay Patonov <patonec@mail.bg>
2025-11-27 21:38:37 +0200

    amend0170001

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { emailValidator } from '../../utils/email.validator';
+import { DOMAINS } from '../../constants';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+    isEditMode: boolean = false;
 
+    form = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(5)]), 
+      email: new FormControl('', [Validators.required, emailValidator(DOMAINS)]), 
+      tel: new FormControl(''), 
+    });
+
+    toggleEditMode(){
+      this.isEditMode = !this.isEditMode;
+    }
+
+    handleSaveProfile(){
+      
+    }
 }
